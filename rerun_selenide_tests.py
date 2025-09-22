249de41daa33ddd1da7e5d4cf4be155ed2ca82ee
Morris <morris.peichl@mineral-minds.de>
2025-09-22 13:10:42 +0200

    Init test 7

--- rerun_selenide_tests.py
+++ rerun_selenide_tests.py
@@ -7,5 +7,14 @@ def set_github_action_output(name, value):
 
 set_github_action_output("test",os.getenv('INPUT_TEST'))
 
-
-
+def get_check_suite_url():
+    if os.getenv('GITHUB_RUN_ATTEMPT') > '1':
+        r = requests.get(f'https://api.github.com/repos/{os.getenv('GITHUB_REPOSITORY')}/actions/runs/{os.getenv('GITHUB_RUN_ID')}/{int(os.getenv('GITHUB_RUN_ATTEMPT')) - 1}',
+                         headers={
+                             'Authorization': f'token {os.getenv("GITHUB_TOKEN")}',
+                             'Accept': 'application/vnd.github+json',
+                             'X-GitHub-Api-Version': '2022-11-28'
+                         })
+        return r.json()['check_suite_url']
+    else:
+        print("::notice title=Not a rerun::Not a rerun")
