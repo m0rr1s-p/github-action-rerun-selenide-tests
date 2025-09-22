5f533e645bae5cf30194c4e33af9574373b1bf5c
Morris <morris.peichl@mineral-minds.de>
2025-09-22 16:14:22 +0200

    test

--- rerun_selenide_tests.py
+++ rerun_selenide_tests.py
@@ -2,6 +2,13 @@ import requests
 import os
 import sys
 
+headers = {
+    'Authorization': f'Bearer {os.getenv("INPUT_GITHUB_TOKEN")}',
+    'Accept': 'application/vnd.github+json',
+    'X-GitHub-Api-Version': '2022-11-28'
+}
+
+
 def set_github_action_output(name, value):
     with open(os.path.abspath(os.environ['GITHUB_OUTPUT']), 'a') as f:
         f.write(f'{name}={value}\n')
@@ -15,35 +22,37 @@ def get_check_suite_url():
         sys.stdout.write(os.linesep)
 
     url = f'https://api.github.com/repos/{os.getenv('GITHUB_REPOSITORY')}/actions/runs/{os.getenv('GITHUB_RUN_ID')}/attempts/{attempt}'
-    r = requests.get(url,
-                     headers={
-                         'Authorization': f'Bearer {os.getenv("INPUT_GITHUB_TOKEN")}',
-                         'Accept': 'application/vnd.github+json',
-                         'X-GitHub-Api-Version': '2022-11-28'
-                     })
+    r = requests.get(url, headers=headers)
     set_github_action_output('check_suite_url', r.json()['check_suite_url'])
     return r.json()['check_suite_url']
 
 
 def get_check_run_annotation_url(check_suite_url):
-    r = requests.get(f'{check_suite_url}/check-runs',
-                     headers={
-                         'Authorization': f'Bearer {os.getenv("INPUT_GITHUB_TOKEN")}',
-                         'Accept': 'application/vnd.github+json',
-                         'X-GitHub-Api-Version': '2022-11-28'
-                     })
+    r = requests.get(f'{check_suite_url}/check-runs', headers=headers)
     for check_run in r.json()['check_runs']:
         if check_run['name'] == os.getenv('INPUT_CHECK_NAME'):
             set_github_action_output('annotations_url', check_run['output']['annotations_url'])
             return check_run['output']['annotations_url']
 
 def get_annotations(annotations_url):
-    r = requests.get(annotations_url,
-                     headers={
-                         'Authorization': f'Bearer {os.getenv("INPUT_GITHUB_TOKEN")}',
-                         'Accept': 'application/vnd.github+json',
-                         'X-GitHub-Api-Version': '2022-11-28'
-                     })
+    r = requests.get(annotations_url, headers=headers)
     return r.json()
 
-get_check_run_annotation_url(get_check_suite_url())
+def create_maven_command():
+    maven_tests_list = []
+    for annotation in annotations:
+        test = annotation['path']
+        method = annotation['title'].split(' ')[0]
+        maven_tests_list.append(f'{test}#{method}')
+        maven_tests_string = ','.join(maven_tests_list)
+    maven_command = f'mvn -D.surefire.rerunFailingTestsCount=2 -Dsurefire.failIfNoSpecifiedTests=false -pl selenide -am -Dtest={maven_tests_string} test -e --settings settings.xml'
+    #set_github_action_output('maven_command', maven_command)
+    print(maven_command)
+    return maven_command
+
+
+check_suite_url = get_check_suite_url()
+annotation_url = get_check_run_annotation_url(check_suite_url)
+annotations = get_annotations(annotation_url)
+if len(annotations) > 0:
+    create_maven_command()
