a32ffd1a0df193c41c872885a2c34b30ee968c98
Morris <morris.peichl@mineral-minds.de>
2025-09-22 14:31:37 +0200

    test

--- rerun_selenide_tests.py
+++ rerun_selenide_tests.py
@@ -6,25 +6,40 @@ def set_github_action_output(name, value):
     with open(os.path.abspath(os.environ['GITHUB_OUTPUT']), 'a') as f:
         f.write(f'{name}={value}\n')
 
-set_github_action_output("test",os.getenv('INPUT_TEST'))
-
 def get_check_suite_url():
     if os.getenv('GITHUB_RUN_ATTEMPT') > '1':
-        # TODO: remove print url
         url = f'https://api.github.com/repos/{os.getenv('GITHUB_REPOSITORY')}/actions/runs/{os.getenv('GITHUB_RUN_ID')}/attempts/{int(os.getenv('GITHUB_RUN_ATTEMPT')) - 1}'
-        print(url)
         r = requests.get(url,
                          headers={
                              'Authorization': f'Bearer {os.getenv("INPUT_GITHUB_TOKEN")}',
                              'Accept': 'application/vnd.github+json',
                              'X-GitHub-Api-Version': '2022-11-28'
                          })
-        # TODO: remove print json
-        print(r.json())
-        set_github_action_output(r.json()['check_suite_url'], os.getenv('INPUT_TEST'))
+        set_github_action_output('check_suite_url', r.json()['check_suite_url'])
         return r.json()['check_suite_url']
     else:
         sys.stdout.write("::notice title=no_attempt_found::No attempt found")
         sys.stdout.write(os.linesep)
 
-get_check_suite_url()
+def get_check_run_annotation_url(check_suite_url):
+    r = requests.get(f'{check_suite_url}/check-runs',
+                     headers={
+                         'Authorization': f'Bearer {os.getenv("INPUT_GITHUB_TOKEN")}',
+                         'Accept': 'application/vnd.github+json',
+                         'X-GitHub-Api-Version': '2022-11-28'
+                     })
+    for check_run in r.json()['check_runs']:
+        if check_run['name'] == os.getenv('INPUT_CHECK_NAME'):
+            set_github_action_output('annotations_url', check_run['annotations_url'])
+            return check_run['annotations_url']
+
+def get_annotations(annotations_url):
+    r = requests.get(annotations_url,
+                     headers={
+                         'Authorization': f'Bearer {os.getenv("INPUT_GITHUB_TOKEN")}',
+                         'Accept': 'application/vnd.github+json',
+                         'X-GitHub-Api-Version': '2022-11-28'
+                     })
+    return r.json()
+
+get_check_run_annotation_url(get_check_suite_url())
