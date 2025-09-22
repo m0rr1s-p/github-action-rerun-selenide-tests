0f6efd30648a634028f3981c6e8cda93fc83e873
Morris <morris.peichl@mineral-minds.de>
2025-09-22 12:07:24 +0200

    Init test 6

--- rerun_selenide_tests.py
+++ rerun_selenide_tests.py
@@ -5,7 +5,7 @@ def set_github_action_output(name, value):
     with open(os.path.abspath(os.environ['GITHUB_OUTPUT']), 'a') as f:
         f.write(f'{name}={value}\n')
 
-set_github_action_output("sum",os.getenv('INPUT_GITHUB_TOKEN'))
+set_github_action_output("test",os.getenv('INPUT_TEST'))
 
 
 
