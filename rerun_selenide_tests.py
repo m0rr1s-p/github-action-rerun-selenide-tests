be4ae47867c102f3d19f681b5fc19b8d8078e3cb
Morris <morris.peichl@mineral-minds.de>
2025-09-22 13:41:15 +0200

    test

--- rerun_selenide_tests.py
+++ rerun_selenide_tests.py
@@ -10,14 +10,16 @@ set_github_action_output("test",os.getenv('INPUT_TEST'))
 
 def get_check_suite_url():
     if os.getenv('GITHUB_RUN_ATTEMPT') > '1':
+        # TODO: remove print url
         url = f'https://api.github.com/repos/{os.getenv('GITHUB_REPOSITORY')}/actions/runs/{os.getenv('GITHUB_RUN_ID')}/attempts/{int(os.getenv('GITHUB_RUN_ATTEMPT')) - 1}'
         print(url)
         r = requests.get(url,
                          headers={
-                             'Authorization': f'token {os.getenv("GITHUB_TOKEN")}',
+                             'Authorization': f'Bearer {os.getenv("GITHUB_TOKEN")}',
                              'Accept': 'application/vnd.github+json',
                              'X-GitHub-Api-Version': '2022-11-28'
                          })
+        # TODO: remove print json
         print(r.json())
         set_github_action_output(r.json()['check_suite_url'], os.getenv('INPUT_TEST'))
         return r.json()['check_suite_url']
